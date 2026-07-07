84f97d50ea7ae70433a0d4a06e11e051eb2ef020
GothamMediaHouse <matthew@gothammediahouse.com>
2026-07-07 11:43:27 -0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Change these values for each event.
-window.GOTHAM_LIVE_CONFIG = {
-  eventTitle: "Client Live Event",
-  eventDescription: "Enter the event password to access the private livestream.",
-  password: "gothamlive",
-  youtubeVideoId: "REPLACE_VIDEO_ID"
-};
